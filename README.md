02ea55982fb2a16f2fc48fa5fcf8d41f6dd991dc
walkinpe19-blip <walkinpe19@gmail.com>
2026-07-13 18:55:03 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Finca Ganadera El Progreso
+
+Proyecto desarrollado utilizando:
+
+- HTML5
+- CSS3
+- JavaScript
+
+Objetivo:
+Desarrollar el sitio web oficial de la Finca Ganadera El Progreso y publicarlo mediante GitHub Pages.
---
+++ js/script.js
@@ -0,0 +1,7 @@
+const boton = document.getElementById("btnContacto");
+
+boton.addEventListener("click", () => {
+
+    alert("Gracias por visitar la Finca Ganadera El Progreso. Próximamente habilitaremos nuestro formulario de contacto.");
+
+});
